bd953e4a911afff4cc1315e0b59e1267196044be
Ben Darlow <github@kapowaz.net>
2026-03-08 23:34:45 +0000

    Add SunIcon and MoonIcon components

---
+++ src/components/icons/MoonIcon.tsx
@@ -0,0 +1,22 @@
+interface MoonIconProps {
+  size?: number;
+  className?: string;
+}
+
+export const MoonIcon = ({ size = 16, className }: MoonIconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    aria-hidden="true"
+    className={className}
+  >
+    <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
+  </svg>
+);
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,8 @@
 export { BrainIcon } from './BrainIcon';
 export { ChevronRightIcon } from './ChevronRightIcon';
+export { MoonIcon } from './MoonIcon';
 export { ShareIcon } from './ShareIcon';
 export { SparklesIcon } from './SparklesIcon';
+export { SunIcon } from './SunIcon';
 export { TrashIcon } from './TrashIcon';
 export { TrendingDownIcon } from './TrendingDownIcon';
