e2ee25bc7fade17e660eaf38a53375bb038deb0c
Ben Darlow <github@kapowaz.net>
2026-03-12 20:02:14 +0000

    Remove unused compare/standings page

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './fonts.css';
 import App from './App';
 import { CompetitionPage } from './CompetitionPage';
 import { IndexRedirect } from './IndexRedirect';
-import { StandingsRenderComparisonPage } from './StandingsRenderComparisonPage';
 import { StandingsImagePage } from './StandingsImagePage';
 
 createRoot(document.getElementById('root')!).render(
@@ -14,7 +13,6 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route element={<App />}>
           <Route index element={<IndexRedirect />} />
-          <Route path="compare/standings" element={<StandingsRenderComparisonPage />} />
           <Route path="standings-image/:slug/" element={<StandingsImagePage />} />
           <Route path=":slug/*" element={<CompetitionPage />} />
         </Route>
